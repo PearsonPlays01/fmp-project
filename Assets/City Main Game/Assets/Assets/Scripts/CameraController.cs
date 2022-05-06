8a761150acd6cc8e716ac4fe026f556fa2dd1ed0
WHE19002530 - Benjamin Wheeler <WHE19002530@wakecoll.net>
2022-05-06 15:45:18 +0100

    Add changes and commit

--- Assets/City Main Game/Assets/Assets/Scripts/CameraController.cs
+++ Assets/City Main Game/Assets/Assets/Scripts/CameraController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+
+        [Header("Movement Limits")]
+    [Space]
+    public bool enableMovementLimits;
     public float moveSpeed;
 
     public float minXRot;
@@ -13,6 +17,11 @@ public class CameraController : MonoBehaviour
 
     public float minZoom;
     public float maxZoom;
+    private Vector3 pos;
+    public Vector2 heightLimit;
+    public Vector2 lenghtLimit;
+    public Vector2 widthLimit;
+    private Vector2 zoomLimit;
 
     public float zoomSpeed;
     public float rotateSpeed;
@@ -48,5 +57,16 @@ public class CameraController : MonoBehaviour
         dir *= moveSpeed * Time.deltaTime;
 
         transform.position += dir;
+
+                if (enableMovementLimits == true)
+        {
+            //movement limits
+            pos = transform.position;
+            pos.y = Mathf.Clamp(pos.y, heightLimit.x, heightLimit.y);
+            pos.z = Mathf.Clamp(pos.z, lenghtLimit.x, lenghtLimit.y);
+            pos.x = Mathf.Clamp(pos.x, widthLimit.x, widthLimit.y);
+            transform.position = pos;
+        }
+        
     }
 }
